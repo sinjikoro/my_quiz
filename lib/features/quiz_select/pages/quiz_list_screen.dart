cf095a6803b1ef22ee4e05c1c198a6f19d09db14
sinjikoro <shinnaga.dev@gmail.com>
2023-10-23 18:34:42 +0900

    fix provider clear

--- lib/features/quiz_select/pages/quiz_list_screen.dart
+++ lib/features/quiz_select/pages/quiz_list_screen.dart
@@ -2,10 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:my_quiz/core/models/question.dart';
 import 'package:my_quiz/core/models/quiz.dart';
-import 'package:my_quiz/features/question/provider/selected_quiz_provider.dart';
 import 'package:my_quiz/features/quiz_select/widgets/quiz_select_area.dart';
 import 'package:my_quiz/features/quiz_select/widgets/welcome_image.dart';
-import 'package:my_quiz/features/result/provider/quiz_result_provider.dart';
 
 enum QuizLevel { easy, normal, hard }
 
@@ -20,8 +18,6 @@ class QuizListScreenState extends ConsumerState<QuizListScreen> {
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
-    ref.read(quizResultProvider.notifier).clear();
-    ref.read(selectedQuizProvider.notifier).clear();
   }
 
   @override
--- lib/features/quiz_select/widgets/quiz_select_area.dart
+++ lib/features/quiz_select/widgets/quiz_select_area.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:my_quiz/core/models/quiz.dart';
 import 'package:my_quiz/features/question/provider/selected_quiz_provider.dart';
+import 'package:my_quiz/features/result/provider/quiz_result_provider.dart';
 
 class QuizSelectArea extends ConsumerWidget {
   const QuizSelectArea(this.quiz, {super.key});
@@ -13,6 +14,8 @@ class QuizSelectArea extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return ElevatedButton(
       onPressed: () {
+        ref.read(quizResultProvider.notifier).clear();
+        ref.read(selectedQuizProvider.notifier).clear();
         ref.read(selectedQuizProvider.notifier).setQuiz(quiz);
         context.push('/quiz/1');
       },
