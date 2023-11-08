d7667af2a11d2585d5b5095b6faa428bcac89c0c
sinjikoro <shinnaga.dev@gmail.com>
2023-11-08 15:23:34 +0900

    del dummy

--- lib/features/quiz_select/pages/quiz_list_screen.dart
+++ lib/features/quiz_select/pages/quiz_list_screen.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:my_quiz/core/models/question.dart';
 import 'package:my_quiz/core/models/quiz.dart';
 import 'package:my_quiz/core/router/app_paths.dart';
 import 'package:my_quiz/features/quiz_select/widgets/quiz_select_area.dart';
@@ -79,32 +78,3 @@ class QuizListScreenState extends ConsumerState<QuizListScreen> {
     return const Center(child: Text('loading error!'));
   }
 }
-
-List<Quiz> _dummyQuizs = [
-  Quiz(title: '初級', description: 'かんたんだよ', questions: _dummyQuestion),
-  Quiz(title: '中級', description: 'ふつうだよ', questions: _dummyQuestion),
-  Quiz(title: '上級', description: 'むずかしいよ', questions: _dummyQuestion),
-];
-
-List<Question> _dummyQuestion = [
-  const Question(
-      id: 1,
-      question: '一番面積の大きい国は？',
-      answers: ['中国', 'アメリカ', 'ロシア'],
-      correctAnswer: 'ロシア'),
-  const Question(
-      id: 2,
-      question: '一番GDPの多い国は？',
-      answers: ['アメリカ', '中国', '日本'],
-      correctAnswer: 'アメリカ'),
-  const Question(
-      id: 3,
-      question: '一番人口の多い国は？',
-      answers: ['インド', 'アメリカ', '中国'],
-      correctAnswer: '中国'),
-  const Question(
-      id: 4,
-      question: '一番南に位置する国は？',
-      answers: ['オーストラリア', '南極', '南アフリカ共和国'],
-      correctAnswer: '南アフリカ共和国'),
-];
