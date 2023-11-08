f5ea5d0f363dc73f36b9cfc641db2baaf9f50ee3
sinjikoro <shinnaga.dev@gmail.com>
2023-11-08 15:23:34 +0900

    del dummy

--- lib/features/quiz_select/pages/quiz_list_screen.dart
+++ lib/features/quiz_select/pages/quiz_list_screen.dart
@@ -79,32 +79,3 @@ class QuizListScreenState extends ConsumerState<QuizListScreen> {
     return const Center(child: Text('loading error!'));
   }
 }
-
-List<Quiz> _dummyQuizs = [
-  Quiz(title: '初級', description: 'かんたんだよ', questions: _dummyQuestion),
-  Quiz(title: '中級', description: 'ふつうだよ', questions: _dummyQuestion),
-  Quiz(title: '上級', description: 'むずかしいよ', questions: _dummyQuestion),
-];
-
-List<Question> _dummyQuestion = [
-  const Question(
-      id: 1,
-      question: '一番面積の大きい国は？',
-      answers: ['中国', 'アメリカ', 'ロシア'],
-      correctAnswer: 'ロシア'),
-  const Question(
-      id: 2,
-      question: '一番GDPの多い国は？',
-      answers: ['アメリカ', '中国', '日本'],
-      correctAnswer: 'アメリカ'),
-  const Question(
-      id: 3,
-      question: '一番人口の多い国は？',
-      answers: ['インド', 'アメリカ', '中国'],
-      correctAnswer: '中国'),
-  const Question(
-      id: 4,
-      question: '一番南に位置する国は？',
-      answers: ['オーストラリア', '南極', '南アフリカ共和国'],
-      correctAnswer: '南アフリカ共和国'),
-];
