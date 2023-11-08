8fba6638c306963692ab91f02aed0acfca08843d
sinjikoro <shinnaga.dev@gmail.com>
2023-11-08 22:51:09 +0900

    a

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,24 +6,42 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:my_quiz/app.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyQuiz());
+  testWidgets('Create quiz', (tester) async {
+    await tester.pumpWidget(const ProviderScope(child: MyQuiz()));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
+    // move create quiz page
     await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    await tester.pumpAndSettle();
+    expect(find.text('Create Quiz'), findsOneWidget);
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // input quiz title
+    await tester.enterText(find.byKey(const Key('title')), 'test title');
+    await tester.pumpAndSettle();
+    expect(find.text('test title'), findsOneWidget);
+
+    // input quiz description
+    await tester.enterText(
+        find.byKey(const Key('description')), 'test description');
+    await tester.pumpAndSettle();
+    expect(find.text('test description'), findsOneWidget);
+
+    // input quiz question
+    await tester.enterText(find.byKey(const Key('question')), 'test question');
+    await tester.pumpAndSettle();
+    expect(find.text('test question'), findsOneWidget);
+
+    // input quiz answer
+    await tester.enterText(find.byKey(const Key('answer')), 'test answer');
+    await tester.pumpAndSettle();
+    expect(find.text('test answer'), findsOneWidget);
+
+    // add quiz
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pumpAndSettle();
   });
 }
