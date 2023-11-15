6118fea11b71fe51ef47d0f1e31779fea6f9e6f1
sinjikoro <shinnaga.dev@gmail.com>
2023-11-16 00:54:17 +0900

    analyze

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
+// import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:my_quiz/app.dart';
-import 'package:my_quiz/core/Provider/mock/quiz_instance_mock.dart';
-import 'package:my_quiz/core/Provider/quiz_instance_provider.dart';
+// import 'package:my_quiz/core/Provider/mock/quiz_instance_mock.dart';
+// import 'package:my_quiz/core/Provider/quiz_instance_provider.dart';
 
 import 'firebase_options.dart';
 
