ce0773789b629341e985765eaa2786c67d405263
sinjikoro <shinnaga.dev@gmail.com>
2023-11-17 01:22:23 +0900

    Add launch configurations for different environments and modes

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,11 +1,10 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
-// import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:my_quiz/app.dart';
-// import 'package:my_quiz/core/Provider/mock/quiz_instance_mock.dart';
-// import 'package:my_quiz/core/Provider/quiz_instance_provider.dart';
+import 'package:my_quiz/core/Provider/mock/quiz_instance_mock.dart';
+import 'package:my_quiz/core/Provider/quiz_instance_provider.dart';
 
 import 'firebase_options.dart';
 
@@ -16,11 +15,16 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
+  final mockValue = await quizInstanceMock();
+
   runApp(
     DevicePreview(
       builder: (context) {
-        return const ProviderScope(
-          child: MyQuiz(),
+        return ProviderScope(
+          overrides: [
+            quizInstanceProvider.overrideWithValue(mockValue),
+          ],
+          child: const MyQuiz(),
         );
       },
     ),
