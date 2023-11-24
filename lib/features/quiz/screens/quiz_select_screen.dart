6b6aa1430b0a93fb80c8e5fbcee77d291271b68d
sinjikoro <shinnaga.dev@gmail.com>
2023-11-25 02:18:31 +0900

    Add Lottie package and update QuizSelectPage snapshot type

--- lib/features/quiz/screens/quiz_select_screen.dart
+++ lib/features/quiz/screens/quiz_select_screen.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:lottie/lottie.dart';
 import 'package:my_quiz/core/Provider/quiz_instance_provider.dart';
 import 'package:my_quiz/core/models/quiz.dart';
 import 'package:my_quiz/core/router/app_paths.dart';
@@ -10,7 +11,6 @@ import 'package:my_quiz/features/quiz/widgets/error_page.dart';
 import 'package:my_quiz/features/quiz/widgets/gradient_card.dart';
 import 'package:my_quiz/features/quiz/widgets/loading_page.dart';
 import 'package:my_quiz/features/quiz/widgets/quiz_select_area.dart';
-import 'package:lottie/lottie.dart';
 
 class QuizSelectScreen extends ConsumerStatefulWidget {
   const QuizSelectScreen({super.key});
@@ -24,14 +24,14 @@ class QuizSelectScreenState extends ConsumerState<QuizSelectScreen> {
   Widget build(BuildContext context) {
     final quizInstance = ref.read(quizInstanceProvider);
 
-    return FutureBuilder(
-      future: quizInstance.get(),
+    return StreamBuilder<QuerySnapshot>(
+      stream: quizInstance.snapshots(),
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.none:
           case ConnectionState.waiting:
-          case ConnectionState.active:
             return const LoadingPage();
+          case ConnectionState.active:
           case ConnectionState.done:
             if (snapshot.hasError) {
               debugPrint(snapshot.error.toString());
@@ -39,7 +39,7 @@ class QuizSelectScreenState extends ConsumerState<QuizSelectScreen> {
             }
 
             return QuizSelectPage(
-              snapshot: snapshot.data,
+              snapshot: snapshot.data as QuerySnapshot<Quiz>?,
             );
         }
       },
